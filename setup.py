8a74744608d1d973962223227757622a122381b9
anakinxc <zhengyancheng.zyc@alibaba-inc.com>
2022-09-20 22:28:46 +0800

    Fix m1 dependencies

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class BuildBazelExtension(build_ext.build_ext):
 
 setup(
     name='secretflow',
-    version='0.7.7b0',
+    version='0.7.7b1',
     license='Apache 2.0',
     description='Secret Flow',
     long_description=long_description,
