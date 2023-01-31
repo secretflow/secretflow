189e1849e5a635c548877769d6322ee1ae55037b
zhouaihui <albert.zah@antgroup.com>
2023-01-31 17:02:14 +0800

    revert setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
+import platform
 import posixpath
 import shutil
+import sys
 from pathlib import Path
 
 import setuptools
@@ -11,11 +13,23 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Default Linux platform tag
+plat_name = "manylinux2014_x86_64"
+
+if sys.platform == "darwin":
+    # Due to a bug in conda x64 python, platform tag has to be 10_16 for X64 wheel
+    if platform.machine() == "x86_64":
+        plat_name = "macosx_10_16_x86_64"
+    else:
+        plat_name = "macosx_11_0_arm64"
+
 
 def read_requirements():
     requirements = []
     with open('./requirements.txt') as file:
         requirements = file.read().splitlines()
+    with open('./docker/dev-requirements.txt') as file:
+        requirements += file.read().splitlines()
     print("Requirements: ", requirements)
     return requirements
 
@@ -105,4 +119,5 @@ setup(
     cmdclass=dict(
         build_ext=BuildBazelExtension, clean=CleanCommand, cleanall=CleanCommand
     ),
+    options={'bdist_wheel': {'plat_name': plat_name}},
 )
