53bc939a52d5394a2a644afd24ae5444901611be
yeyeyang <1063162407@qq.com>
2023-08-15 18:17:38 +0800

    Add encoding to finite field functions

--- secretflow/utils/ndarray_encoding.py
+++ secretflow/utils/ndarray_encoding.py
@@ -15,7 +15,60 @@
 import numpy as np
 
 from .errors import InvalidArgumentError
+def LWE_encode(m: np.ndarray, is_float:bool,fxp_bits: int,prime:int) -> np.ndarray:
+    """Encode float ndarray to uint64 finite field.
+    Float will times 2**fxp_bits firstly.
 
+    Args:
+        m (np.ndarray): the ndarray to encode.
+        is_float: the tpye of the ndarray is float.
+        fraction_precesion (int): keep how many decimal digits after the dot.
+            Must provide if ndarray dtype is float.          ##小数点后面的精度
+
+    Returns:
+        np.ndarray: the encoded FieldArray.
+    """
+    assert isinstance(m, np.ndarray), f'Support ndarray only but got {type(m)}'
+    abs_max = (prime - 1) / 2
+    if is_float:
+        if m.dtype not in [np.float16, np.float32, np.float64]:
+            raise InvalidArgumentError(f'Accept float ndarray only but got {m.dtype}')
+        assert fxp_bits is not None, f'Fxp_bits must not be None.'
+        max_value = abs(m).max()
+        if abs(max_value * (1 << fxp_bits)) > abs_max:
+            raise InvalidArgumentError(
+                f'Float data {max_value} exceeds uint range ({-abs_max}, {abs_max}) after encoding.')
+        # Convert to np.float64 for reducing overflow.% GF.order
+        nparray_data = ((m.astype(np.float64) * (1 << fxp_bits)) % prime).astype(np.int64)
+    else:
+        max_value = abs(m).max()
+        if abs(max_value) > abs_max:
+            raise InvalidArgumentError(
+                f'Float data {max_value} exceeds uint range ({-abs_max}, {abs_max}) after encoding.')
+        nparray_data = (m % prime).astype(np.int64)
+    return nparray_data
+
+def LWE_decode(m: np.ndarray, is_float: bool,fxp_bits: int,prime:int) -> np.ndarray:
+    """Decode ndarray from uint64 finite field to the float.
+    Fraction precesion shall be corresponding to encoding fraction precesion.
+
+    Args:
+        m (np.ndarray): the ndarray to decode.
+        fxp_bits (int): the decimal digits to keep when encoding float.
+            Must provide if the original dtype is float.
+
+    Returns:
+        np.ndarray: the decoded float ndarray.
+    """
+    assert isinstance(m, np.ndarray), f'Support ndarray only but got {type(m)}'
+    for i in range(len(m)):
+        if m[i] > ((prime - 1) / 2):
+            m[i] = m[i] - prime
+    if is_float:
+        assert fxp_bits is not None, f'Fraction precesion must not be None.'
+        return m.astype(np.int64) / (1 << fxp_bits)
+    else:
+        return m.astype(np.int64)
 
 def encode(m: np.ndarray, fxp_bits: int) -> np.ndarray:
     """Encode float ndarray to uint64 finite field.
