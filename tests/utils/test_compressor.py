edfe00b11e225c11411a22417952c9d07adf2c5d
ues1df <3373182599@qq.com>
2023-08-16 16:17:27 +0800

    update: 修改测试代码格式

--- tests/utils/test_compressor.py
+++ tests/utils/test_compressor.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-from secretflow.utils.compressor import ForwardData, QuantizedData, QuantizedFP
+from secretflow.utils.compressor import (ForwardData, QuantizedData,
+                                         QuantizedFP, QuantizedKmeans)
 
 
-def test_compressed(compressor):
-    a = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+def compressed_test(compressor):
+    a = np.random.normal(0, 5, size=(128, 256))
     c_a = compressor.compress(a)
     assert isinstance(c_a, QuantizedData)
 
@@ -17,26 +18,28 @@ def test_compressed(compressor):
     assert isinstance(c_a_forward.hidden, QuantizedData)
 
 
-def test_abs_max_equal(compressor):
-    a = np.array([0, -100.0, 1, 2, 3, 4, -5, -6, -7, -8])
+def abs_max_equal_test(compressor):
+    a = np.random.normal(0, 5, size=(128, 256))
     c_a = compressor.compress(a)
     res = compressor.decompress(c_a)
     np.testing.assert_almost_equal(np.max(np.abs(res)), np.max(np.abs(a)))
 
 
-def test_all_zeros(compressor):
-    a = np.zeros([3, 3])
+def all_zeros_test(compressor):
+    a = np.zeros((128, 256))
     c_a = compressor.compress(a)
     res = compressor.decompress(c_a)
     assert (res == a).all()
 
 
-def test_qfp_main():
+def test_qfp():
     compressor = QuantizedFP()
-    test_compressed(compressor)
-    test_abs_max_equal(compressor)
-    test_all_zeros(compressor)
+    compressed_test(compressor)
+    abs_max_equal_test(compressor)
+    all_zeros_test(compressor)
 
 
-if __name__ == '__main__':
-    test_qfp_main()
+def test_qkm():
+    compressor = QuantizedKmeans()
+    compressed_test(compressor)
+    all_zeros_test(compressor)
