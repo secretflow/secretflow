997e60fa4c1546bd33d4bd6f4f88bbad6e1b4cfb
mia <36161957+imogenxingren@users.noreply.github.com>
2023-10-14 22:23:59 +0800

    upload additional util file(#896)

---
+++ secretflow/security/aggregation/experiment/h_nn_prune/dgl_utils.py
@@ -0,0 +1,200 @@
+"""
+    Auxiliary code: Helper functions for deep gradient leakage attack simulation.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from secretflow.ml.nn.utils import BaseModule, TorchModel
+
+
+# label to onehot
+def label_to_onehot(target, num_classes=100):
+    target = torch.unsqueeze(target, 1)
+    onehot_target = torch.zeros(target.size(0), num_classes, device=target.device)
+    onehot_target.scatter_(1, target, 1)
+    return onehot_target
+
+
+# cross entropy
+def cross_entropy_for_onehot(pred, target):
+    return torch.mean(torch.sum(-target * F.log_softmax(pred, dim=-1), 1))
+
+
+# init weights
+def weights_init(m):
+    if hasattr(m, "weight"):
+        m.weight.data.uniform_(-0.5, 0.5)
+    if hasattr(m, "bias"):
+        m.bias.data.uniform_(-0.5, 0.5)
+
+
+class ConvNet(BaseModule):
+    """Small ConvNet for MNIST."""
+
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=3)
+        self.fc_in_dim = 192
+        self.fc = nn.Linear(self.fc_in_dim, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 3))
+        x = x.view(-1, self.fc_in_dim)
+        x = self.fc(x)
+        return F.softmax(x, dim=1)
+
+
+class LeNet(BaseModule):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        act = nn.Sigmoid
+        self.body = nn.Sequential(
+            nn.Conv2d(1, 12, kernel_size=5, padding=5 // 2, stride=2),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=2),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=1),
+            act(),
+        )
+        self.fc = nn.Sequential(nn.Linear(768, 100))
+
+    def forward(self, x):
+        out = self.body(x)
+        out = out.view(out.size(0), -1)
+        # print(out.size())
+        out = self.fc(out)
+        return out
+
+
+# BasicBlock
+class BasicBlock(BaseModule):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(self.expansion * planes),
+            )
+
+    def forward(self, x):
+        out = F.Sigmoid(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.Sigmoid(out)
+        return out
+
+
+# Bottleneck
+class Bottleneck(BaseModule):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(
+            planes, self.expansion * planes, kernel_size=1, bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(self.expansion * planes),
+            )
+
+    def forward(self, x):
+        out = F.Sigmoid(self.bn1(self.conv1(x)))
+        out = F.Sigmoid(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.Sigmoid(out)
+        return out
+
+
+# ResNet
+class ResNet(BaseModule):  # nn.Module
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=1)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=1)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.Sigmoid(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+# ResNet18()
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+# ResNet34
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+# ResNet34
+def ResNet34():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+# ResNet101
+def ResNet101():
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
+
+# ResNet152
+def ResNet152():
+    return ResNet(Bottleneck, [3, 8, 36, 3])
