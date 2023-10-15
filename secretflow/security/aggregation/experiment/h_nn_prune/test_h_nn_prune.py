57d2c7fd6869265149a42e1e3f261a46ca62d354
mia <36161957+imogenxingren@users.noreply.github.com>
2023-10-15 22:24:40 +0800

    Update test_h_nn_prune.py(#896)

--- secretflow/security/aggregation/experiment/h_nn_prune/test_h_nn_prune.py
+++ secretflow/security/aggregation/experiment/h_nn_prune/test_h_nn_prune.py
@@ -12,8 +12,7 @@ from torchmetrics import Accuracy, Precision
 from torchvision import datasets, models, transforms
 
 import secretflow as sf
-from dgl_utils import (ConvNet, LeNet, ResNet, ResNet34,
-                       cross_entropy_for_onehot, label_to_onehot)
+from dgl_utils import ConvNet, LeNet, ResNet, ResNet34
 from secretflow.ml.nn import FLModel
 from secretflow.ml.nn.fl.utils import metric_wrapper, optim_wrapper
 from secretflow.ml.nn.utils import BaseModule, TorchModel
@@ -72,10 +71,10 @@ start_time = time.time()
 # plt.ylabel('Accuracy')
 # plt.xlabel('Epoch')
 # plt.legend(['Train', 'Valid'], loc='upper left')
-# plt.savefig('FL_Model_accuracy_convnet_20_32_mnist.jpg')
+# plt.savefig('FL_Model_accuracy_convnet_10_32_mnist.jpg')
 # plt.show()
-#
-# # training result with pruning model parameters
+
+# training result with pruning model parameters
 fl_model_prune = FLModel(
     server=server,
     device_list=device_list,
@@ -106,18 +105,9 @@ plt.legend(["Train", "Valid"], loc="upper left")
 plt.savefig("./dgl/FL_prune_0.1_5_Model_accuracy_convnet_10_32_mnist.jpg")
 plt.show()
 
-
 # dgl attack verification
-# tt = transforms.ToPILImage()
-# img_index = 56  # image_index
-# gt_data = tp(dst[img_index][0])  # true data
-# gt_data = gt_data.view(1, *gt_data.size())
-# gt_label = torch.Tensor([dst[img_index][1]]).long()  # true label
-# gt_label = gt_label.view(
-#     1,
-# )
-# gt_onehot_label = label_to_onehot(gt_label)  # one-hot label
-# plt.imshow(tt(gt_data[0].cpu()))
+tt = transforms.ToPILImage()
+torch.manual_seed(1234)
 
 def weights_init(m):
     if hasattr(m, "weight"):
@@ -127,88 +117,72 @@ def weights_init(m):
 
 gt_data = torch.load("./dgl/sf_output/x.pt")
 gt_label = torch.load("./dgl/sf_output/y.pt")
-tp = transforms.Compose([transforms.ToPILImage()])
-plt.imshow(tp(gt_data[0].cpu()),cmap='gray')
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
+plt.imshow(tt(gt_data[0].cpu()), cmap='gray')
 plt.savefig("./dgl/true_to_valiate.jpg", dpi=300)
-net = LeNet()  # original model
-torch.manual_seed(1234)
-net.apply(weights_init)  # init param
-criterion = nn.CrossEntropyLoss()
 
-tt = transforms.ToPILImage()
-pred = net(gt_data)
-y = criterion(pred, gt_label)
-dy_dx = torch.autograd.grad(y, net.parameters())
-# original dgl without pruning
-original_dy_dx = list((_.detach().clone() for _ in dy_dx))  # true gradient
+net = LeNet()
+original_dy_dx = []
 
-# generate dummy data and label
-num_classes = 10
-dummy_data = torch.randn(gt_data.size()).requires_grad_(True)
-dummy_label = torch.randn((gt_data.shape[0], num_classes)).requires_grad_(True)
-plt.imshow(tt(dummy_data[0].cpu()))
-plt.savefig("./dgl/dummy.jpg", dpi=300)
-
-# define dgl optimizer
-LR = 1.0
-ITERATION = 300
-optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
-
-is_prune = False
+# flag: if the gradient is from model with purning
+is_prune = True
 if is_prune:
-    original_dy_dx =[]
     for i in range(4):
         tens = torch.from_numpy(np.load("./dgl/sf_output/gradients" + str(i) + ".npy"))
         original_dy_dx.append(tens)
+else:
+    net.apply(weights_init)  # init param
+    criterion = nn.CrossEntropyLoss()  # criterion
+    pred = net(gt_data)
+    y = criterion(pred, gt_label)
+    dy_dx = torch.autograd.grad(y, net.parameters())
+    original_dy_dx = list((_.detach().clone() for _ in dy_dx))  # true gradient
 
-# dgl attack without pruning
+# generate dummy data and label
+dummy_data = torch.randn(gt_data.size()).requires_grad_(True)
+dummy_label = torch.randn(gt_label.size()).requires_grad_(True)
+plt.imshow(tt(dummy_data[0].cpu()), cmap='gray')
+plt.savefig("./dgl/dummy.jpg", dpi=300)
+
+# define dgl optimizer
+optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
+
+# dgl attack
 history = []
-history_iters = []
-grad_difference = []
-data_difference = []
-current_loss = 0
-fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-for iters in range(ITERATION):
+for iters in range(300):
     def closure():  # optimize dummy data, calculate dummy grad, grad diff
         optimizer.zero_grad()
         dummy_pred = net(dummy_data)
-        dummy_loss = -torch.mean( # diff between pred dummy and dummy label
-            torch.sum(torch.softmax(dummy_label, -1) * torch.log(torch.softmax(dummy_pred, -1)), dim=-1))
-        dummy_dy_dx = torch.autograd.grad(dummy_loss, net.parameters(), create_graph=True)
+        dummy_onehot_label = F.softmax(dummy_label, dim=-1)
+        dummy_loss = criterion(
+            dummy_pred, dummy_onehot_label
+        )  # diff between pred dummy and dummy label
+        dummy_dy_dx = torch.autograd.grad(
+            dummy_loss, net.parameters(), create_graph=True
+        )  # dummy gradient
         grad_diff = 0
-        for gx, gy in zip(dummy_dy_dx, original_dy_dx):  # diff between true gradient and dummy gradient
+        for gx, gy in zip(
+            dummy_dy_dx, original_dy_dx
+        ):  # diff between true gradient and dummy gradient
             grad_diff += ((gx - gy) ** 2).sum()
         grad_diff.backward()  # backward gradient diff
         return grad_diff
 
     optimizer.step(closure)  # optimize closure (gradient diff)
-    current_loss = closure().item()
-    grad_difference.append(current_loss)
-    data_difference.append(torch.mean((dummy_data - gt_data) ** 2).item())
-    current_time = str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
-    print(current_time, iters, 'grad diff = %.8f, data diff = %.8f' % (current_loss, data_difference[-1]))
-    ax1.plot(grad_difference)
-    fig1.savefig("dgl/current_loss_" + str(LR) + "_" + "LBFGS" + ".jpg")
-    ax2.plot(data_difference)
-    fig2.savefig("dgl/data_difference_" + str(LR) + "_" + "LBFGS" + ".jpg")
-
     if iters % 10 == 0:  #
-        history.append([tp(dummy_data[0].cpu())])
-        history_iters.append(iters)
+        current_loss = closure()
+        print(iters, "%.4f" % current_loss.item())
+        history.append(tt(dummy_data[0].cpu()))
 
-        plt.figure(figsize=(12, 8))
-        plt.subplot(3, 10, 1)
-        plt.imshow(tp(gt_data[0].cpu()), cmap='gray')
-        for i in range(min(len(history), 29)):
-            plt.subplot(3, 10, i + 2)
-            # plt.imshow(history[i][imidx])
-            plt.imshow(history[i][0], cmap='gray')
-            plt.title('iter=%d' % (history_iters[i]))
-            plt.axis('off')
-        if is_prune:
-            plt.savefig("dgl/contrast_after_prune.jpg", dpi=300)
-        else:
-            plt.savefig("dgl/contrast_before_prune.jpg", dpi=300)
-        plt.close()
-
+# plot the process of dummy data
+plt.figure(figsize=(12, 8))
+for i in range(30):
+    plt.subplot(3, 10, i + 1)
+    plt.imshow(history[i],cmap='gray')
+    plt.title("iter=%d" % (i * 10))
+    plt.axis("off")
+if is_prune:
+    plt.savefig("./dgl/contrast_after_prune.jpg", dpi=300)
+else:
+    plt.savefig("./dgl/contrast_before_prune.jpg", dpi=300)
