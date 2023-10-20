53c4620fa456b7480386a32aaa9b8f30bd6970d8
liuzhihao <1348990147@qq.com>
2023-10-20 11:09:40 +0800

    8

--- secretflow/security/aggregation/experiment/mask_heu_aggregator.py
+++ secretflow/security/aggregation/experiment/mask_heu_aggregator.py
@@ -1,4 +1,3 @@
-# 思路:生成mask，向梯度中加入mask，同态加密mask，同态聚合mask，解密聚合mask，聚合带mask梯度，得到梯度zero-mask聚合值
 from functools import partial
 from typing import Any, Dict, List, Tuple, Union
 
@@ -11,9 +10,9 @@ from secretflow.device.device.heu import HEUMoveConfig
 from secretflow.security.aggregation import Aggregator
 
 
-# 定义一个类，用来进行同态加密
+#This class defines the behavior of participants, where mask is added to some positions of the gradient to protect gradient information
 @proxy(PYUObject)
-class masker:
+class Masker:
     def __init__(self, participant, shape=None) -> None:
         self._participants = participant
         self.shape = shape
@@ -22,23 +21,22 @@ class masker:
         return self.mask
 
     def add_noisy(self, data, weight=None, mask_location=-2):
-        # 生成噪声self.mask，以及带有噪声的梯度data，他们都乘以了weight
-        # data是梯度向量，是一个list[numpyarray],mask_location表示噪声添加的位置
+        # 'data 'is a gradient vector, a list [numpyarray] or ndarray, mask_location represents the location where mask is added
         islist = isinstance(data, list)
         if not islist:
             data = [data]
         if len(data) < 2:
             mask_location = 0
-        data1 = data[mask_location]  # 获取需要保护的位置
-        flatten_data = data1.flatten()  # 将张量展开以便于添加噪声
+        protect_data = data[mask_location]  #  Obtain the location that needs protection 
+        flatten_data = protect_data.flatten()  #  Expanding tensors for adding mask
         data_len = len(flatten_data)
         mask = np.random.randint(1, 10, data_len)
         if weight is None:
             weight = 1
         self.mask = mask * weight
-        for i in range(data_len):  # 添加噪声
+        for i in range(data_len):  # add mask
             flatten_data[i] = flatten_data[i] + mask[i]
-        final_data = flatten_data.reshape(data1.shape)
+        final_data = flatten_data.reshape(protect_data.shape)
         data[mask_location] = final_data
         for i in range(len(data)):
             data[i] = data[i] * weight
@@ -48,23 +46,61 @@ class masker:
             return data[0]
 
 
-class mask_heu_aggregator(Aggregator):
+class MaskHeuAggregator(Aggregator):
+    """
+    Mask_ Heu_ Aggregation presents a new federated learning secure aggregation algorithm, in which
+    participants add mask at some positions of the gradient (the fully connected layer closest to the
+    output layer), Then use homomorphic encryption to remove masks from the aggregated values  
+
+    The specific process is as follows.
+
+    1. After the participant generates the gradient locally,they add a mask at some positions of the
+    gradient (the fully connected layer closest to the output layer) and use paillier homomorphic encryption
+    on the mask
+
+    2. Participants will send gradients with masks and encrypted masks to the server for aggregation
+
+    3. The server aggregates gradients using addition, aggregates masks in ciphertext state using homomorphic
+    addition, and finally returns the aggregation results to the participants
+
+    4. The participant decrypts the aggregated values of the mask and ultimately obtains a noise free aggregation result
+
+    NOTES:
+     Analyzing the reasons for gradient leakage of privacy information, we found that certain positions of the
+    gradient contain sensitive information. Based on the above findings, we designed this aggregation algorithm
+    For more information, please refer to https://openaccess.thecvf.com/content/CVPR2021/papers/Sun_Soteria_Provable_Defense_Against_Privacy_Leakage_in_Federated_Learning_From_CVPR_2021_paper.pdf
+    and https://arxiv.org/abs/2001.02610
+
+    Examples:
+        >>> # Alice and bob are both pyu instances.
+        >>> aggregator = MaskHeuAggregator(alice, [alice, bob])
+        >>> a = alice(lambda :  np.array([[1, 2, 3]]))()
+        >>> b = bob(lambda : np.array([[11, 12, 13]]))()
+        >>> avg_a_b = aggregator.average([a, b], axis=0)
+        >>> # Get the result.
+        >>> sf.reveal(avg_a_b)
+            array([[6. 7. 8.]])
+        >>> sum_a_b = aggregator.sum([a, b], axis=0)
+        >>> sf.reveal(sum_a_b)
+            array([[12. 14. 16.]])
+    """
     def __init__(
-        self, device: PYU, participants: List[PYU], fxp_bits: int = 18, mask_location=-2
+        self, device: PYU, participants: List[PYU], mask_location=-2
     ):
         assert len(set(participants)) == len(
             participants
         ), 'Should not have duplicated devices.'
-        self._device = device
-        self._participants = set(participants)
+        self._device = device # The server is used to aggregate gradients 
+        self._participants = set(participants)# The participant
         self.sk_keeper = participants[0]
         self.evaluators = participants[1:]
         self.evaluators.append(device)
-        self._fxp_bits = fxp_bits
         self.mask_location = mask_location
-        # mask_location表示添加噪声的位置，梯度是一个由多个nparray构成的列表，每个nparray表示神经网络中的某一层，该算法向最后一个全连接层添加噪声
-        # 下面代码用于构建同态加密设备
-        sk_keeper = {'party': self.sk_keeper.party}
+        # gradient is a list composed of multiple ndarrays,
+        # each representing a certain layer in a neural network.
+        # The algorithm adds mask to the last fully connected layer
+        # self.mask_location represents the position of the last fully connected layer
+        sk_keeper = {'party': self.sk_keeper.party}# build HEU device
         evaluators = [{'party': evaluator.party} for evaluator in self.evaluators]
         heu_config = {
             'sk_keeper': sk_keeper,
@@ -76,19 +112,19 @@ class mask_heu_aggregator(Aggregator):
             },
         }
         self.heu_device = sf.HEU(heu_config, spu.spu_pb2.FM128)
-        # 为每个参与方分配了同态加密设备
-        self._masker = {
-            pyu: masker(participant=pyu.party, shape=None, device=pyu)
+        
+        self._Masker = {
+            pyu: Masker(participant=pyu.party, shape=None, device=pyu)
             for pyu in participants
-        }
+        }# Build entities for each participant 
 
     def average(self, data: List[DeviceObject], axis=None, weights=None):
         def remove_mask(
             masked_data: List[DeviceObject], mask=None, weight=None, mask_location=-2
         ):
+            #The server obtains gradient with mask and aggregate values with mask, ultimately obtaining an aggregate result without mask
             is_list = isinstance(masked_data[0], list)
             sum_weight = np.sum(weight, axis=axis) if weight else len(masked_data)
-            # 下面是聚合以及去噪操作
             if is_list:
                 results = [np.sum(element, axis=axis) for element in zip(*masked_data)]
                 if len(sf.reveal(results)) < 2:
@@ -105,7 +141,6 @@ class mask_heu_aggregator(Aggregator):
                 results = results / sum_weight
                 return results
 
-        # 向每个参与方的梯度添加噪声
         _weight = []
         if weights is not None and isinstance(weights, (list, tuple, np.ndarray)):
             assert len(weights) == len(
@@ -120,34 +155,34 @@ class mask_heu_aggregator(Aggregator):
                 else:
                     _weight.append(w)
             for i, (datum, weight) in enumerate(zip(data, weights)):
-                data[i] = self._masker[data[i].device].add_noisy(
+                data[i] = self._Masker[data[i].device].add_noisy(
                     data=datum, weight=weight, mask_location=self.mask_location
                 )
         else:
             for i, datum in enumerate(data):
-                data[i] = self._masker[data[i].device].add_noisy(
+                data[i] = self._Masker[data[i].device].add_noisy(
                     data=datum, weight=weights, mask_location=self.mask_location
                 )
 
-        masked_data = [d.to(self._device) for d in data]  # 将带有噪声的梯度传输至服务器
-        # 对每个参与方的mask进行同态加密
+        masked_data = [d.to(self._device) for d in data]  #  Transfer gradient with mask to server 
+        #  Perform homomorphic encryption on the masks of each participant 
         encrypted_mask = [None] * len(data)
         for i, datum in enumerate(data):
             encrypted_mask[i] = (
-                self._masker[datum.device]
+                self._Masker[datum.device]
                 .get_mask()
                 .to(
                     self.heu_device,
                     config=HEUMoveConfig(heu_dest_party=self._device.party),
                 )
             )
-        # 对同态加密的mask进行聚合
+        #  Aggregate the encryption results 
         for i in range(len(data)):
             if i == 0:
                 mask_result = encrypted_mask[0]
             else:
                 mask_result = mask_result + encrypted_mask[i]
-        mask_result = mask_result.to(self.sk_keeper)  # 这一步是进行解密操作
+        mask_result = mask_result.to(self.sk_keeper)  # decrypt 
         mask_result = mask_result.to(self._device)
         final_result = self._device(remove_mask)(
             masked_data,
@@ -173,30 +208,29 @@ class mask_heu_aggregator(Aggregator):
                 results = results - mask
                 return results
 
-        # 将每个参与方的梯度添加噪声
         for i in range(len(data)):
-            data[i] = self._masker[data[i].device].add_noisy(
+            data[i] = self._Masker[data[i].device].add_noisy(
                 data[i], mask_location=self.mask_location
             )
-        masked_data = [d.to(self._device) for d in data]  # 将数据传输至服务器
-        # 对每个参与方的mask进行同态加密
+        masked_data = [d.to(self._device) for d in data]  
+ 
         encrypted_mask = [None] * len(data)
         for i, datum in enumerate(data):
             encrypted_mask[i] = (
-                self._masker[datum.device]
+                self._Masker[datum.device]
                 .get_mask()
                 .to(
                     self.heu_device,
                     config=HEUMoveConfig(heu_dest_party=self._device.party),
                 )
             )
-        # 对同态加密的mask进行聚合
+
         for i in range(len(data)):
             if i == 0:
                 mask_result = encrypted_mask[0]
             else:
                 mask_result = mask_result + encrypted_mask[i]
-        mask_result = mask_result.to(self.sk_keeper)  # 这一步是进行解密操作
+        mask_result = mask_result.to(self.sk_keeper)  
         mask_result = mask_result.to(self._device)
         final_result = self._device(remove_mask)(masked_data, mask=mask_result)
         return final_result
--- tests/security/aggregation/test_mask_heu_aggregator.py
+++ tests/security/aggregation/test_mask_heu_aggregator.py
@@ -1,15 +1,15 @@
 import pytest
 
-from secretflow.security.aggregation.experiment.mask_heu_aggregation import (
-    mask_heu_aggregator,
+from secretflow.security.aggregation.experiment.mask_heu_aggregator import (
+    MaskHeuAggregator,
 )
 from tests.security.aggregation.test_aggregator_base import AggregatorBase
 
 
-class Testmask_heu_aggregator(AggregatorBase):
+class TestMaskHeuAggregator(AggregatorBase):
     @pytest.fixture()
     def env_and_aggregator(self, sf_production_setup_devices):
-        yield sf_production_setup_devices, mask_heu_aggregator(
+        yield sf_production_setup_devices, MaskHeuAggregator(
             sf_production_setup_devices.carol,
             [sf_production_setup_devices.alice, sf_production_setup_devices.bob],
         )
