370e9872b27cbe5be129821c2648279b3727c84a
UMR99 <1486147921@qq.com>
2023-12-22 14:37:55 +0800

    在 SecretFlow 中实现经典联邦策略 SCAFFOLD

--- tests/ml/nn/fl/strategy/test_scaffold_torch.py
+++ tests/ml/nn/fl/strategy/test_scaffold_torch.py
@@ -30,9 +30,9 @@ class My_Model(BaseModule):
         x = self.head(x)
         return x
 
-class TestFedPer(unittest.TestCase):
-    def test_fed_per_local_step(self):
-        # Initialize FedPer strategy with ConvNet model
+class TestScaffold(unittest.TestCase):
+    def test_scaffold_local_step(self):
+        # Initialize Scaffold strategy with ConvNet model
         class ConvNetBuilder:
             def __init__(self):
                 # 初始化时，定义一个空的指标列表
@@ -54,30 +54,30 @@ class TestFedPer(unittest.TestCase):
 
         # 使用 ConvNetBuilder 初始化 FedPer
         conv_net_builder = ConvNetBuilder()
-        fed_per_worker = Scaffold(builder_base=conv_net_builder)
+        scaffold_worker = Scaffold(builder_base=conv_net_builder)
 
         # Prepare dataset
         x_test = torch.rand(128, 1, 28, 28)  # Randomly generated data
         y_test = torch.randint(0, 10, (128,))  # Randomly generated labels for a 10-class task
         test_loader = DataLoader(TensorDataset(x_test, y_test), batch_size=32, shuffle=True)
-        fed_per_worker.train_set = iter(test_loader)
-        fed_per_worker.train_iter = iter(fed_per_worker.train_set)
+        scaffold_worker.train_set = iter(test_loader)
+        scaffold_worker.train_iter = iter(scaffold_worker.train_set)
 
         # Perform a training step
         gradients = None
-        gradients, num_sample = fed_per_worker.train_step(
+        gradients, num_sample = scaffold_worker.train_step(
             gradients, cur_steps=0, train_steps=1
         )
 
         # Apply weights update
-        fed_per_worker.apply_weights(gradients)
+        scaffold_worker.apply_weights(gradients)
 
         # Assert the sample number and length of gradients
         self.assertEqual(num_sample, 32)  # Batch size
-        self.assertEqual(len(gradients), len(list(fed_per_worker.model.parameters())))  # Number of model parameters
+        self.assertEqual(len(gradients), len(list(scaffold_worker.model.parameters())))  # Number of model parameters
 
         # Perform another training step to test cumulative behavior
-        _, num_sample = fed_per_worker.train_step(gradients, cur_steps=1, train_steps=2)
+        _, num_sample = scaffold_worker.train_step(gradients, cur_steps=1, train_steps=2)
         self.assertEqual(num_sample, 64)  # Cumulative batch size over two steps
 
 if __name__ == '__main__':
