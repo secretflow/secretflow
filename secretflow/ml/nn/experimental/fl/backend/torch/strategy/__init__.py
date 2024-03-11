29ddeb2a114ff33335a47cf056321a565564dcef
zhangxingmeng <103552200+zhangxingmeng@users.noreply.github.com>
2024-03-11 17:28:26 +0800

    [reorg]move fedper to experimental and fix  (#1188)
    * modify single model doc
    
    * add index
    
    * format
    
    * update format
    
    * update
    
    * update
    
    * resolve cr comment
    
    * fix error and move to experimental

--- secretflow/ml/nn/experimental/fl/backend/torch/strategy/__init__.py
+++ secretflow/ml/nn/experimental/fl/backend/torch/strategy/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .fed_per import PYUFedPer
+
+__all__ = [
+    'PYUFedPer',
+]
