44b46e34c0d66299cc77450bca1569e77a04f4ff
Zhulin An <zan@nvidia.com>
2024-04-06 22:51:36 +0800

    add class CNN_FMNIST and class CifarCNN

--- secretflow/ml/nn/core/torch/module.py
+++ secretflow/ml/nn/core/torch/module.py
@@ -243,7 +243,76 @@ class BaseModule(ParametersMixin, nn.Module):
             else:
                 m.update(y_pred, y_true.int())
 
+import torch.nn.functional as F
+class CifarCNN(BaseModule):
+    def __init__(self, num_classes=10, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, padding=0)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.fc1 = nn.Linear(64 * 3 * 3, 128)
+        self.fc2 = nn.Linear(128, num_classes, bias=True)
+        self.base_weight_keys = [
+                                'conv1.weight', 'conv1.bias',
+                                'conv2.weight', 'conv2.bias',
+                                'conv3.weight', 'conv3.bias',
+                                'fc1.weight', 'fc1.bias',
+                                ]
+        self.classifier_weight_keys = [
+                                'fc2.weight', 'fc2.bias',
+                                ]
 
+    def forward(self, x):
+        x = self.pool(F.leaky_relu(self.conv1(x)))
+        x = self.pool(F.leaky_relu(self.conv2(x)))
+        x = self.pool(F.leaky_relu(self.conv3(x)))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.leaky_relu(self.fc1(x))
+        y = self.fc2(x)
+        return x, y
+    
+    def feature2logit(self, x):
+        return self.fc2(x)
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+class CNN_FMNIST(BaseModule):
+    def __init__(self, num_classes=10, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, padding=0)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5, padding=1)
+        self.fc1 = nn.Linear(32 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, num_classes, bias=True)
+        self.base_weight_keys = ['conv1.weight', 'conv1.bias',
+                            'conv2.weight', 'conv2.bias',
+                            'fc1.weight', 'fc1.bias',]
+        self.classifier_weight_keys = ['fc2.weight', 'fc2.bias',]
+
+    def forward(self, x):
+        x = self.pool(F.leaky_relu(self.conv1(x)))
+        x = self.pool(F.leaky_relu(self.conv2(x)))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.leaky_relu(self.fc1(x))
+        y = self.fc2(x)
+        return x, y
+
+    def feature2logit(self, x):
+        return self.fc2(x)
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+    
 class TorchModel:
     def __init__(
         self,
