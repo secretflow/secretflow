175672e9971e87fa212132bad39409bfaf6c3b3b
UMR99 <1486147921@qq.com>
2024-04-09 18:53:19 +0800

    在 SecretFlow 中实现经典联邦策略 FedDYN

--- tests/ml/nn/fl/strategy/test_fed_dyn_torch.py
+++ tests/ml/nn/fl/strategy/test_fed_dyn_torch.py
@@ -1,3 +1,17 @@
+# Copyright chenyufan, chenyufan_22@163.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 import torch.optim as optim
 from secretflow.ml.nn.fl.backend.torch.strategy.fed_dyn import FedDYN
@@ -69,7 +83,9 @@ class TestFedDYN:
 
         # Assert the sample number and length of gradients
         assert num_sample == 32  # Batch size
-        assert len(gradients) == len(list(fed_dyn_worker.model.parameters()))  # Number of model parameters
+        assert len(gradients) == len(
+            list(fed_dyn_worker.model.parameters())
+        )  # Number of model parameters
 
         # Perform another training step to test cumulative behavior
         _, num_sample = fed_dyn_worker.train_step(gradients, cur_steps=1, train_steps=2)
