72a5e6776de8fecdfc073304112547ee6d5b5d88
wangzul <wangzuli163@163.com>
2024-04-11 15:38:26 +0800

    ARM适配 ci test #调整build.sh 构建镜像的过程 #优化release-config.yml 中arm代码逻辑

--- docker/release/build.sh
+++ docker/release/build.sh
@@ -88,7 +88,7 @@ docker buildx build \
   --build-arg deploy_templates="$(cat deploy_templates.yml)" \
   --build-arg comp_list="$(cat comp_list.json)" \
   --build-arg translation="$(cat translation.json)" \
-  . --load
+  . --push
 
 #Output construction completion information
 echo -e "Finish building ${GREEN}${IMAGE_TAG} for linux/arm64 and linux/amd64${NO_COLOR}"
@@ -103,20 +103,32 @@ docker buildx build \
   --build-arg deploy_templates="$(cat deploy_templates.yml)" \
   --build-arg comp_list="$(cat comp_list.json)" \
   --build-arg translation="$(cat translation.json)" \
-  . --load
+  . --push
 
 #Output construction completion information
 echo -e "Finish building ${GREEN}${IMAGE_LITE_TAG} for linux/arm64 and linux/amd64${NO_COLOR}"
 
-# Push multi platform mirroring to Docker Hub
-docker push ${IMAGE_TAG}
-docker push ${IMAGE_LITE_TAG}
-
 if [[ LATEST -eq 1 ]]; then
     echo -e "Tag and push ${GREEN}${LATEST_TAG}${NO_COLOR} ..."
-    docker tag ${IMAGE_TAG} ${LATEST_TAG}
-    docker push ${LATEST_TAG}
+docker buildx build \
+  --platform linux/arm64,linux/amd64 \
+  -f anolis.Dockerfile \
+  -t ${LATEST_TAG} \
+  --build-arg sf_version=${VERSION} \
+  --build-arg config_templates="$(cat config_templates.yml)" \
+  --build-arg deploy_templates="$(cat deploy_templates.yml)" \
+  --build-arg comp_list="$(cat comp_list.json)" \
+  --build-arg translation="$(cat translation.json)" \
+  . --push
     echo -e "Tag and push ${GREEN}${LATEST_LITE_TAG}${NO_COLOR} ..."
-    docker tag ${IMAGE_LITE_TAG} ${LATEST_LITE_TAG}
-    docker push ${LATEST_LITE_TAG}
+docker buildx build \
+  --platform linux/arm64,linux/amd64 \
+  -f anolis-lite.Dockerfile . \
+  -t ${LATEST_LITE_TAG} \
+  --build-arg sf_version=${VERSION} \
+  --build-arg config_templates="$(cat config_templates.yml)" \
+  --build-arg deploy_templates="$(cat deploy_templates.yml)" \
+  --build-arg comp_list="$(cat comp_list.json)" \
+  --build-arg translation="$(cat translation.json)" \
+  . --push
 fi
