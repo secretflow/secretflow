75dc42e703cff69831fe7bcde78627625359e572
qxzhou1010 <qxzhou1010@qq.com>
2024-08-09 17:31:55 +0800

    Decrease png size

--- docs/developer/benchmark/resources/generate_psi.py
+++ docs/developer/benchmark/resources/generate_psi.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Ant Group Co., Ltd.
+# Copyright 2024 Ant Group Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
