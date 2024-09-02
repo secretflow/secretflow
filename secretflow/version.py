aec95de655f98227e533e66186239e2a052bdc86
jiangzc86 <zhicheng.jzc@alibaba-inc.com>
2024-09-02 15:12:03 +0800

    Update version.py

--- secretflow/version.py
+++ secretflow/version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.9.0b0"
+__version__ = "1.9.0b1"
 __commit_id__ = "$$COMMIT_ID$$"
 __docker_version__ = "$$DOCKER_VERSION$$"
 __build_time__ = "$$BUILD_TIME$$"
