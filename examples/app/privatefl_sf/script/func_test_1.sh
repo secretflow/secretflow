173afa35ca46a6dabaae21e49b4437231c8f114d
ZhangLei2474 <129866462+ZhangLei2474@users.noreply.github.com>
2024-11-01 15:00:53 +0800

    Delete examples/app/privatefl_sf/script directory

--- examples/app/privatefl_sf/script/func_test_1.sh
+++
@@ -1,3 +0,0 @@
-echo "==> Start Test"
-python ../FedAverage.py --data='mnist' --nclient=2 --nclass=10 --ncpc=5 --model='mnist_fully_connected_IN' --mode='LDP' --round=1 --epsilon=2 --sr=1 --lr=0.1
-echo "==> Test Finished!"
--- examples/app/privatefl_sf/script/install_conda.sh
+++
@@ -1,5 +0,0 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-py38_23.3.1-0-Linux-x86_64.sh &&
-bash Miniconda3-py38_23.3.1-0-Linux-x86_64.sh
-
-# Follow the prompts on the installer screens to install, then CLOSE and then RE-OPEN your terminal window.
-# see details in https://docs.conda.io/projects/conda/en/latest/user-guide/install/linux.html
