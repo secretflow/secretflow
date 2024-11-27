7d3e082765cbbc950f68c44997a9880e79514aaf
xwr0709 <166706306+xwr0709@users.noreply.github.com>
2024-11-27 14:43:07 +0800

    Update fed_dyn.py

--- secretflow/ml/nn/fl/backend/torch/strategy/fed_dyn.py
+++ secretflow/ml/nn/fl/backend/torch/strategy/fed_dyn.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
-# *_* coding: utf-8 *_*
-
-# Copyright 2022 Ant Group Co., Ltd.
+# Copyright 2024 Ant Group Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
