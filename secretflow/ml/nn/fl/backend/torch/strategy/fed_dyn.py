57bdd1263b11d66877f2c80ebc970f8301e0287e
xwr0709 <166706306+xwr0709@users.noreply.github.com>
2024-11-30 11:11:22 +0800

    Update fed_dyn.py

--- secretflow/ml/nn/fl/backend/torch/strategy/fed_dyn.py
+++ secretflow/ml/nn/fl/backend/torch/strategy/fed_dyn.py
@@ -18,8 +18,8 @@ from typing import Tuple
 import numpy as np
 
 import torch
-from secretflow.ml.nn.fl.backend.torch.fl_base import BaseTorchModel
-from secretflow.ml.nn.fl.strategy_dispatcher import register_strategy
+from secretflow_fl.ml.nn.fl.backend.torch.fl_base import BaseTorchModel
+from secretflow_fl.ml.nn.fl.strategy_dispatcher import register_strategy
 
 
 class FedDYN(BaseTorchModel):
