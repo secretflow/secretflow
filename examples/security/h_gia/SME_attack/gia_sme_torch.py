fa6730256c5d44168bc7465404e626e58aea0fcd
skylineZSS <sszhang@seu.edu.cn>
2024-12-06 14:37:37 +0800

    move the execution of the SME attack to the attacker's end

--- examples/security/h_gia/SME_attack/gia_sme_torch.py
+++ examples/security/h_gia/SME_attack/gia_sme_torch.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Ant Group Co., Ltd.
+# Copyright 2024 Ant Group Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,39 +11,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logging
-from typing import Callable, Dict, List
-
-import numpy as np
-import torch
-from torchvision.transforms import ToPILImage
-import os
 import copy
 import json
-from scipy.optimize import linear_sum_assignment
-import matplotlib.pyplot as plt
+import logging
+import os
+from typing import Callable, Dict, List
 
-from secretflow import reveal
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from scipy.optimize import linear_sum_assignment
+from torchvision.transforms import ToPILImage
+
+from secretflow import reveal, wait
+from secretflow.device import PYU, DeviceObject, PYUObject
 from secretflow_fl.ml.nn.callbacks.attack import AttackCallback
 from secretflow_fl.ml.nn.fl.backend.torch.fl_base import BaseTorchModel
+
 
 def require_grad(net, flag):
     for p in net.parameters():
         p.require_grad = flag
 
+
 def prior_boundary(data, low, high):
     with torch.no_grad():
         data.data = torch.clamp(data, low, high)
+
 
 def compute_norm(inputs):
     squared_sum = sum([p.square().sum() for p in inputs])
     norm = squared_sum.sqrt()
     return norm
 
+
 def total_variation(x):
     dh = (x[:, :, :, :-1] - x[:, :, :, 1:]).abs().mean()
     dw = (x[:, :, :-1, :] - x[:, :, 1:, :]).abs().mean()
     return (dh + dw) / 2
+
 
 def psnr(data, rec, sort=False):
     assert data.max().item() <= 1.0001 and data.min().item() >= -0.0001
@@ -51,14 +57,15 @@ def psnr(data, rec, sort=False):
     cost_matrix = []
     if sort:
         for x_ in rec:
-            cost_matrix.append(
-                [(x_ - d).square().mean().item() for d in data]
-            )
+            cost_matrix.append([(x_ - d).square().mean().item() for d in data])
         row_ind, col_ind = linear_sum_assignment(cost_matrix)
         assert np.all(row_ind == np.arange(len(row_ind)))
         data = data[col_ind]
-    psnr_list = [10 * np.log10(1 / (d - r).square().mean().item()) for d, r in zip(data, rec)]
+    psnr_list = [
+        10 * np.log10(1 / (d - r).square().mean().item()) for d, r in zip(data, rec)
+    ]
     return np.mean(psnr_list)
+
 
 def save_figs(tensors, path, subdir=None, dataset=None):
     def save(imgs, path):
@@ -68,18 +75,30 @@ def save_figs(tensors, path, subdir=None, dataset=None):
             plt.axis('off')
             plt.savefig(os.path.join(path, f'{name}.png'), bbox_inches='tight')
             plt.close()
+
     tensor2image = ToPILImage()
     path = os.path.join(path, subdir)
     os.makedirs(path, exist_ok=True)
     if dataset == "FEMNIST":
         tensors = 1 - tensors
     imgs = [
-        [i, tensor2image(tensors[i].detach().cpu().squeeze())] for i in range(len(tensors))
+        [i, tensor2image(tensors[i].detach().cpu().squeeze())]
+        for i in range(len(tensors))
     ]
     save(imgs, path)
 
 
+# AttackCallback of SME
 class GiadentInversionAttackSME(AttackCallback):
+    """
+    Implemention of SME attack algorithm in paper Surrogate model extension (SME): a fast and accurate weight update attack on federated learning: https://dl.acm.org/doi/abs/10.5555/3618408.3620229. SME attack is an improvement of the gradient inversion attack DLG. A surrogate model method is proposed in SME based on the characteristic of two-dimensional gradient flow and low-rank property of local updates. SME can largely boost the ability of gradient inversion attacks on weight updates containing many iterations and achieve state-of-the-art (SOTA) performance.
+
+    Attributes:
+        attack_party: attack party.
+        victim_party: victim party.
+        attack_configs: detailed configurations of SME attack.
+
+    """
 
     def __init__(self, attack_party, victim_party, attack_configs, **kwargs):
         super().__init__(**kwargs)
@@ -91,118 +110,141 @@ class GiadentInversionAttackSME(AttackCallback):
 
     def on_train_begin(self, logs=None):
         def get_victim_model(victim_worker: BaseTorchModel):
-            print('get victim model before training')
             return victim_worker.model
-        
-        #get victim model before training
-        self.victim_model1 = copy.deepcopy(reveal(self._workers[self.victim_party].apply(get_victim_model)))
 
-        return 
+        # get victim model before training and send model data to attack party
+        self.victim_model1 = copy.deepcopy(self._workers[self.victim_party].apply(get_victim_model)).to(self.attack_party)
+ 
+        return
 
     def on_train_end(self, logs=None):
         def get_victim_model(victim_worker: BaseTorchModel):
-            print('get victim model after training')
             return victim_worker.model
-        
+
         def get_victim_trainloader(victim_worker: BaseTorchModel):
             return victim_worker.train_set
-        
-        victim_trainloader = reveal(self._workers[self.victim_party].apply(get_victim_trainloader))
 
-        self.victim_model2 = copy.deepcopy(reveal(self._workers[self.victim_party].apply(get_victim_model)))
-        
+        # the raw data of victim is only used to evaluate the attack performance
+        # the attacker has no access to the raw data in practical scenario
+        self.victim_trainloader = self._workers[self.victim_party].apply(get_victim_trainloader).to(self.attack_party)
 
-        setup = {"device": "cuda", "dtype": torch.float32}
-        path_to_res="./res"
-        attacker = SME(
-            trainloader=victim_trainloader,
-            setup=setup,
-            alpha=self.attack_configs['alpha'],
-            test_steps=self.attack_configs['test_steps'],
-            path_to_res=self.attack_configs['path_to_res'],
-            lamb=self.attack_configs['lamb'],
-            mean_std=(0., 1.),
-        )
-
-        attacker.net0 = self.victim_model1
-        attacker.net1 = self.victim_model2
+        # get victim model after training and send model data to attack party
+        self.victim_model2 = self._workers[self.victim_party].apply(get_victim_model).to(self.attack_party)
 
         # Reconstruction
-        def gia_sme_attack(sme_attacker: SME):
-            stats = sme_attacker.reconstruction(
-                eta=self.attack_configs['eta'],
-                beta=self.attack_configs['beta'],
-                iters=self.attack_configs['iters'],
-                lr_decay=self.attack_configs['lr_decay'],
-                save_figure=self.attack_configs['save_figure'],
-            )
-            with open(os.path.join(path_to_res, "res.json"), "w") as f:
-                json.dump(stats, f, indent=4)
-            
-            return stats
+        attacker = SME(
+                setup=self.attack_configs['setup'],
+                alpha=self.attack_configs['alpha'],
+                test_steps=self.attack_configs['test_steps'],
+                path_to_res=self.attack_configs['path_to_res'],
+                lamb=self.attack_configs['lamb'],
+                mean_std=(0.0, 1.0),
+                )
         
-        smetrics = gia_sme_attack(attacker)
-    
+        def gia_sme_attack(attacker: SME, attack_configs: dict, victim_model1, victim_model2, victim_trainloader) -> PYUObject:
+            attack_configs = PYUObject(self.attack_party, attack_configs)
+            def _gia_sme_attack(attacker, attack_configs, victim_model1, victim_model2, victim_trainloader):
+
+                attacker.net0 = victim_model1
+                attacker.net1 = victim_model2
+                attacker.trainloader = victim_trainloader
+
+                stats = attacker.reconstruction(
+                    eta=attack_configs['eta'],
+                    beta=attack_configs['beta'],
+                    iters=attack_configs['iters'],
+                    lr_decay=attack_configs['lr_decay'],
+                    save_figure=attack_configs['save_figure'],
+                )
+
+                with open(os.path.join(attack_configs['path_to_res'], "res.json"), "w") as f:
+                    json.dump(stats, f, indent=4)
+                return stats
+            
+            # conduct the attack func on attack party (bob)
+            wait(self.attack_party(_gia_sme_attack)(attacker, attack_configs, victim_model1, victim_model2, victim_trainloader))
+            return 
+        
+        gia_sme_attack(attacker, self.attack_configs, self.victim_model1, self.victim_model2, self.victim_trainloader)
+        return
+
     def get_attack_metrics(self):
         return self.metrics
 
-
+# the class of SME attack
 class SME:
     def __init__(
-            self,
-            trainloader,
-            setup,
-            alpha,
-            test_steps,
-            path_to_res,
-            mean_std,
-            lamb,
-            dataset=None,
+        self,
+        setup,
+        alpha,
+        test_steps,
+        path_to_res,
+        mean_std,
+        lamb,
+        dataset=None,
     ):
-        self.alpha = torch.tensor(alpha, requires_grad=True, **setup)
+        self.alpha = alpha
         self.rec_alpha = 0 < self.alpha < 1
         self.setup = setup
         self.net0 = None
         self.net1 = None
+        self.trainloader = None
         self.test_steps = test_steps
         os.makedirs(path_to_res, exist_ok=True)
         self.path = path_to_res
         self.lamb = lamb
         self.dataset = dataset
+
+        self.data = None
+        self.x = None
+        self.y = None
+
+        self.mean = torch.tensor(mean_std[0]).reshape(1, -1, 1, 1)
+        self.std = torch.tensor(mean_std[1]).reshape(1, -1, 1, 1)
+
+    def reconstruction(
+        self, eta, beta, iters, lr_decay, signed_grad=False, save_figure=True
+    ):
+
         data, labels = [], []
-        for img, l in trainloader:
+        for img, l in self.trainloader:
             labels.append(l)
             data.append(img)
-        self.data = torch.cat(data).to(**setup)
+
+        self.data = torch.cat(data).to(self.setup['device'])
 
         # We assume that labels have been restored separately, for details please refer to the paper.
-        self.y = torch.cat(labels).to(device=setup["device"])
+        self.y = torch.cat(labels).to(device=self.setup['device'])
         # Dummy input.
-        self.x = torch.normal(0, 1, size=self.data.shape, requires_grad=True, **setup)
+        self.x = torch.normal(0, 1, size=self.data.shape, requires_grad=True, device=self.setup['device'])
 
-        self.mean = torch.tensor(mean_std[0]).to(**setup).reshape(1, -1, 1, 1)
-        self.std = torch.tensor(mean_std[1]).to(**setup).reshape(1, -1, 1, 1)
+        self.mean = self.mean.to(self.setup['device'])
+        self.std = self.std.to(self.setup['device'])
         # This is a trick (a sort of prior information) adopted from IG.
         prior_boundary(self.x, -self.mean / self.std, (1 - self.mean) / self.std)
 
-    def reconstruction(self, eta, beta, iters, lr_decay, signed_grad=False, save_figure=True):
+
         self.net0.to(device=self.setup['device'])
         self.net1.to(device=self.setup['device'])
+        
         # when taking the SME strategy, alpha is set within (0, 1).
+        self.alpha = torch.tensor(self.alpha, requires_grad=True, device=self.setup['device'])
         if 0 < self.alpha < 1:
-            self.alpha.grad = torch.tensor(0.).to(**self.setup)
+            self.alpha.grad = torch.tensor(0.0).to(**self.setup)
+        
         optimizer = torch.optim.Adam(params=[self.x], lr=eta)
         alpha_opti = torch.optim.Adam(params=[self.alpha], lr=beta)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
-                                                         milestones=[iters // 2.667,
-                                                                     iters // 1.6,
-                                                                     iters // 1.142],
-                                                         gamma=0.1)
-        alpha_scheduler = torch.optim.lr_scheduler.MultiStepLR(alpha_opti,
-                                                               milestones=[iters // 2.667,
-                                                                           iters // 1.6,
-                                                                           iters // 1.142],
-                                                               gamma=0.1)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer,
+            milestones=[iters // 2.667, iters // 1.6, iters // 1.142],
+            gamma=0.1,
+        )
+        alpha_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            alpha_opti,
+            milestones=[iters // 2.667, iters // 1.6, iters // 1.142],
+            gamma=0.1,
+        )
+        
         criterion = torch.nn.CrossEntropyLoss(reduction="mean")
 
         # Direction of the weight update.
@@ -218,7 +260,9 @@ class SME:
         with torch.no_grad():
             _net = copy.deepcopy(self.net0)
             _net.to(device=self.setup['device'])
-            for p, q, z in zip(self.net0.parameters(), self.net1.parameters(), _net.parameters()):
+            for p, q, z in zip(
+                self.net0.parameters(), self.net1.parameters(), _net.parameters()
+            ):
                 z.data = (1 - self.alpha) * p + self.alpha * q
 
         # Reconstruction
@@ -232,7 +276,11 @@ class SME:
             if self.rec_alpha:
                 # Update the surrogate model.
                 with torch.no_grad():
-                    for p, q, z in zip(self.net0.parameters(), self.net1.parameters(), _net.parameters()):
+                    for p, q, z in zip(
+                        self.net0.parameters(),
+                        self.net1.parameters(),
+                        _net.parameters(),
+                    ):
                         z.data = (1 - self.alpha) * p + self.alpha * q
             pred = _net(self.x)
             loss = criterion(input=pred, target=self.y)
@@ -241,9 +289,7 @@ class SME:
             grad = [p / norm for p in grad]
 
             # Compute x's grad.
-            cos_loss = 1 - sum([
-                p.mul(q).sum() for p, q in zip(w1_w0, grad)
-            ])
+            cos_loss = 1 - sum([p.mul(q).sum() for p, q in zip(w1_w0, grad)])
             loss = cos_loss + self.lamb * total_variation(self.x)
             loss.backward()
             if signed_grad:
@@ -252,10 +298,12 @@ class SME:
             # Compute alpha's grad.
             if self.rec_alpha:
                 with torch.no_grad():
-                    for p, q, z in zip(self.net0.parameters(), self.net1.parameters(), _net.parameters()):
-                        self.alpha.grad += z.grad.mul(
-                            q.data - p.data
-                        ).sum()
+                    for p, q, z in zip(
+                        self.net0.parameters(),
+                        self.net1.parameters(),
+                        _net.parameters(),
+                    ):
+                        self.alpha.grad += z.grad.mul(q.data - p.data).sum()
                 if signed_grad:
                     self.alpha.grad.sign_()
 
@@ -272,17 +320,27 @@ class SME:
                     _x = self.x * self.std + self.mean
                     _data = self.data * self.std + self.mean
                 measurement = psnr(_data, _x, sort=True)
-                print(f"iter: {i}| alpha: {self.alpha.item():.2f}| (1 - cos): {cos_loss.item():.3f}| "
-                      f"psnr: {measurement:.3f}")
-                stats.append({
-                    "iter": i,
-                    "alpha": self.alpha.item(),
-                    "cos_loss": cos_loss.item(),
-                    "psnr": measurement,
-                })
+                print(
+                    f"iter: {i}| alpha: {self.alpha.item():.2f}| (1 - cos): {cos_loss.item():.3f}| "
+                    f"psnr: {measurement:.3f}"
+                )
+                stats.append(
+                    {
+                        "iter": i,
+                        "alpha": self.alpha.item(),
+                        "cos_loss": cos_loss.item(),
+                        "psnr": measurement,
+                    }
+                )
                 if save_figure:
-                    save_figs(tensors=_x, path=self.path, subdir=str(i), dataset=self.dataset)
+                    save_figs(
+                        tensors=_x, path=self.path, subdir=str(i), dataset=self.dataset
+                    )
         if save_figure:
-            save_figs(tensors=self.data * self.std + self.mean,
-                      path=self.path, subdir="original", dataset=self.dataset)
+            save_figs(
+                tensors=self.data * self.std + self.mean,
+                path=self.path,
+                subdir="original",
+                dataset=self.dataset,
+            )
         return stats
--- tests/ml/nn/fl/attack/test_torch_gia_sme.py
+++ tests/ml/nn/fl/attack/test_torch_gia_sme.py
@@ -14,39 +14,22 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from examples.security.h_gia.SME_attack.gia_sme_torch import GiadentInversionAttackSME
 from torch import optim
 from torchmetrics import Accuracy, Precision
 
 import secretflow as sf
-from secretflow_fl.ml.nn import FLModel
 from secretflow.security.aggregation import SecureAggregator
-from secretflow.utils.simulation.datasets import load_mnist
+from secretflow_fl.utils.simulation.datasets_fl import load_mnist
+from secretflow_fl.ml.nn import FLModel
 from secretflow_fl.ml.nn.core.torch import (
-    metric_wrapper,
-    optim_wrapper,
     BaseModule,
     TorchModel,
+    metric_wrapper,
+    optim_wrapper,
 )
 
-from gia_sme_torch import GiadentInversionAttackSME
-
-attack_configs = {
-    "path_to_res": "./examples/security/h_gia/SME_attack/res",
-    "dataset": "MNIST",
-    "k": 50,
-    "batchsize": 10,
-    "epochs": 20,
-    "alpha": 0.5,
-    "lamb": 0.01,
-    "train_lr": 0.004,
-    "eta": 1,
-    "beta": 0.001,
-    "iters": 1000,
-    "test_steps": 50,
-    "lr_decay": True,
-    "save_figure": True
-}
-
+# the global model
 class FLBaseNet(BaseModule):
     def __init__(self, classes=10):
         super(FLBaseNet, self).__init__()
@@ -69,23 +52,16 @@ class FLBaseNet(BaseModule):
         return self.fc2(x)
 
 
-def do_test_fl_and_gia_sme():
-    print('The version of SecretFlow: {}'.format(sf.__version__))
+def do_test_fl_and_gia_sme(attack_configs: dict ,alice, bob):
 
-    sf.shutdown()
-
-    sf.init(['alice', 'bob'], address='local')
-    alice, bob = sf.PYU('alice'), sf.PYU('bob')
-
-    #prepare dataset
+    # prepare dataset
     client_data_num = attack_configs['k']
     (train_data, train_label), (test_data, test_label) = load_mnist(
-        parts={alice: (0,client_data_num)},
+        parts={alice: (0, client_data_num)},
         normalized_x=True,
         categorical_y=True,
         is_torch=True,
     )
-    
 
     loss_fn = nn.CrossEntropyLoss
     optim_fn = optim_wrapper(optim.Adam, lr=attack_configs['train_lr'])
@@ -94,12 +70,14 @@ def do_test_fl_and_gia_sme():
         loss_fn=loss_fn,
         optim_fn=optim_fn,
         metrics=[
-            metric_wrapper(Accuracy, task="multiclass", num_classes=10, average='micro'),
-            metric_wrapper(Precision, task="multiclass", num_classes=10, average='micro'),
-            ],
-        )
-
-    
+            metric_wrapper(
+                Accuracy, task="multiclass", num_classes=10, average='micro'
+            ),
+            metric_wrapper(
+                Precision, task="multiclass", num_classes=10, average='micro'
+            ),
+        ],
+    )
 
     device_list = [alice]
     server = bob
@@ -116,10 +94,9 @@ def do_test_fl_and_gia_sme():
         # use_gpu=True,
     )
 
+    # conduct SME attack through callback
     gia_callback = GiadentInversionAttackSME(
-        attack_party=bob,
-        victim_party=alice,
-        attack_configs = attack_configs
+        attack_party=bob, victim_party=alice, attack_configs=attack_configs
     )
 
     history = fl_model.fit(
@@ -128,10 +105,40 @@ def do_test_fl_and_gia_sme():
         epochs=attack_configs['epochs'],
         batch_size=attack_configs['batchsize'],
         aggregate_freq=1,
-        callbacks=[gia_callback]
+        callbacks=[gia_callback],
     )
-
 
     return
 
-do_test_fl_and_gia_sme()
+# attack configurations
+attack_configs = {
+    "path_to_res": "./examples/security/h_gia/SME_attack/res",
+    "dataset": "MNIST",
+    "k": 50,
+    "batchsize": 10,
+    "epochs": 20,
+    "alpha": 0.5,
+    "lamb": 0.01,
+    "train_lr": 0.004,
+    "eta": 1,
+    "beta": 0.001,
+    "iters": 1000,
+    "test_steps": 100,
+    "lr_decay": True,
+    "save_figure": True,
+    "setup": {"device": "cpu", "dtype": torch.float32}
+}
+
+
+def test_fl_and_gia_sme(sf_simulation_setup_devices):
+    alice = sf_simulation_setup_devices.alice
+    bob = sf_simulation_setup_devices.bob
+    do_test_fl_and_gia_sme(
+        attack_configs, alice, bob
+    )
+
+# sf.init(['alice', 'bob', 'charlie'], address='local', debug_mode=False)
+# alice, bob = sf.PYU('alice'), sf.PYU('bob')
+# do_test_fl_and_gia_sme(
+#         attack_configs, alice, bob
+#     )
