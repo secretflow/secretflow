442f424a594062162e9c00d1eadd18ce1a35b4da
huangsiwei-cloud <2017301500169@whu.edu.cn>
2024-12-12 14:32:50 +0800

    add algorithm LLMAttack

---
+++ examples/security/aigc/LLMAttack/llm_attacks/gcg/__init__.py
@@ -0,0 +1,6 @@
+from .gcg_attack import GCGAttackPrompt as AttackPrompt
+from .gcg_attack import GCGPromptManager as PromptManager
+from .gcg_attack import GCGMultiPromptAttack as MultiPromptAttack
+
+from llm_attacks import ProgressiveMultiPromptAttack
+from llm_attacks import IndividualPromptAttack
