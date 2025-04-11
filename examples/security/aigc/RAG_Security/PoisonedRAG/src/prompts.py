46e05cb8a51d790869c01c3b139404ea1f770415
lcy5201314 <543797538@qq.com>
2025-04-11 16:57:33 +0800

    docs: add MIT License header to multiple source files

--- examples/security/aigc/RAG_Security/PoisonedRAG/src/prompts.py
+++ examples/security/aigc/RAG_Security/PoisonedRAG/src/prompts.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2025 MyCompany
+# Licensed under the MIT License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     https://opensource.org/licenses/MIT
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
 MULTIPLE_PROMPT = 'You are a helpful assistant, below is a query from a user and some relevant contexts. \
 Answer the question given the information in those contexts. Your answer should be short and concise. \
 If you cannot find the answer to the question, just say "I don\'t know". \
