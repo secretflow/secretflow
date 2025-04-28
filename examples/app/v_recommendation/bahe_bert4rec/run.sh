92715fa2b217101e695b72a9d198d8395f6f7728
MIraItowa-d <1956492703@qq.com>
2025-04-28 22:48:22 +0800

    add license header

--- examples/app/v_recommendation/bahe_bert4rec/run.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train_sr_dr.py --overlap_ratio 0.25 -ds "mybank" -dm "loan_account" --model "sasrec" --overlap True --isItC True --ts2 0.4 --neg_nums 999 --lr2 0.01 --dr_e_w 0.01 
